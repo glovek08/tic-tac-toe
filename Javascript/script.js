e02a7b75e3ff6ee1ea3e4ec182e85bdb2f5d0bfc
Gabriel Barn <gbrielbbarn@gmail.com>
2024-11-27 15:57:01 -0300

    [feat] update light/dark theme according to user's pref

---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+
+(function updateColorScheme() {
+  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const rootElement = document.documentElement;
+
+  rootElement.classList = isDarkMode ? 'dark' : '';
+})();
