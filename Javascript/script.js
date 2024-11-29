d4acd9433754ea2cd648104091eb23b3f3a74a6a
Gabriel Barn <gbrielbbarn@gmail.com>
2024-11-29 18:48:01 -0300

    [feat] testing project

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,20 @@
 
-(function updateColorScheme() {
-  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const rootElement = document.documentElement;
-  rootElement.classList = isDarkMode ? 'dark' : '';
-})();
+// (function updateColorScheme() {
+//   const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+//   const rootElement = document.documentElement;
+//   rootElement.classList = isDarkMode ? 'dark' : '';
+// })();
+
+
+
+
+
+function Gameboard() {
+  const board = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ];
+  //outer array = rows.
+  //inner array = column.
+  const getBoard = () => board;
+}
+
+//each cell could be an object containing information like its index, cell value (empty, player or bot). 
+
