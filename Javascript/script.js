a1a3813e5dee921a659eea7735b7e13ed30f1063
Gabriel Barn <gbrielbbarn@gmail.com>
2024-12-02 17:52:06 -0300

    [feat] dynamically generate board, winning combinations array

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,20 +1,39 @@
-
 // (function updateColorScheme() {
 //   const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 //   const rootElement = document.documentElement;
 //   rootElement.classList = isDarkMode ? 'dark' : '';
 // })();
 
-
-
-
-
 function Gameboard() {
-  const board = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ];
-  //outer array = rows.
-  //inner array = column.
-  const getBoard = () => board;
+  let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  //    0 1 2
+  //    3 4 5
+  //    6 7 8
+
+  const WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], //rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], //columns
+    [0, 4, 8],
+    [2, 4, 6], //diagonals
+  ];
+
+  return {
+    generateBoard: function () {
+      const gameboardDiv = document.querySelector("#gameboard");
+      //for each element in board, we create a new button element that will store its index in a dataset.
+      board.forEach((el) => {
+        const gameboardCell = document.createElement("button");
+        gameboardCell.dataset.index = board.indexOf(el);
+        gameboardCell.classList.add("cell");
+        gameboardDiv.appendChild(gameboardCell);
+      });
+    },
+  };
 }
 
-//each cell could be an object containing information like its index, cell value (empty, player or bot). 
-
+//each cell could be an object containing information like its index, cell value (empty, player or bot).
