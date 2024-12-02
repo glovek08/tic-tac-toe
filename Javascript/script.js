47b33ea91fd5957c17e8cadf68d1cc6898232d31
Gabriel Barn <gbrielbbarn@gmail.com>
2024-12-02 18:51:53 -0300

    [fix] dataset.index using the index argument of forEach inside generateBoard

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,11 @@
-// (function updateColorScheme() {
-//   const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-//   const rootElement = document.documentElement;
-//   rootElement.classList = isDarkMode ? 'dark' : '';
-// })();
+(function updateColorScheme() {
+  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const rootElement = document.documentElement;
+  rootElement.classList = isDarkMode ? 'dark' : '';
+})();
 
 function Gameboard() {
+
   let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
   //    0 1 2
@@ -12,28 +13,24 @@ function Gameboard() {
   //    6 7 8
 
   const WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8], //rows
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8], //columns
-    [0, 4, 8],
-    [2, 4, 6], //diagonals
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+    [0, 4, 8], [2, 4, 6], //diagonals
   ];
 
   return {
     generateBoard: function () {
       const gameboardDiv = document.querySelector("#gameboard");
       //for each element in board, we create a new button element that will store its index in a dataset.
-      board.forEach((el) => {
+      board.forEach((el, index) => {
         const gameboardCell = document.createElement("button");
-        gameboardCell.dataset.index = board.indexOf(el);
+        gameboardCell.dataset.index = index;
         gameboardCell.classList.add("cell");
         gameboardDiv.appendChild(gameboardCell);
       });
     },
   };
 }
+Gameboard().generateBoard();
 
 //each cell could be an object containing information like its index, cell value (empty, player or bot).
