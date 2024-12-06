5db654bba26465b3d6a5320a43d6d93c3d10b98d
Gabriel Barn <gbrielbbarn@gmail.com>
2024-12-06 16:48:15 -0300

    [feat] win logic

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,44 +1,64 @@
 (function updateColorScheme() {
-  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const rootElement = document.documentElement;
-  rootElement.classList = isDarkMode ? 'dark' : '';
+  rootElement.classList = isDarkMode ? "dark" : "";
 })();
 
 function Gameboard() {
-  let currentTurn = 'player-turn';
+  let currentTurn = "player-turn";
   let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
   //    0 1 2
   //    3 4 5
   //    6 7 8
+  const WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], //rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], //columns
+    [0, 4, 8],
+    [2, 4, 6], //diagonals
+  ];
 
   const setBoardValue = (cellIndex) => {
-    if(currentTurn === 'player-turn') {
+    if (currentTurn === "player-turn") {
       board[cellIndex] = 1;
       console.log(board);
+      checkWin();
       changeTurn();
     } else {
       board[cellIndex] = 2;
-      changeTurn();
       console.log(board);
+      checkWin();
+      changeTurn();
     }
-  }
+  };
 
-  const changeTurn = () => {
-    if (currentTurn === 'player-turn') {
-      currentTurn = 'bot-turn';
-      return;
-    } else {
-      currentTurn = 'player-turn';
-      return;
+  const checkWin = () => {
+    console.log("gameboard inside checkWin: " + board);
+    for (let combination of WIN_COMBINATIONS) {
+      if (
+        board[combination[0]] === 1 &&
+        board[combination[1]] === 1 &&
+        board[combination[2]] === 1
+      ) {
+        window.alert("PLAYER WINS!");
+        return;
+      } else if (
+        board[combination[0]] === 2 &&
+        board[combination[1]] === 2 &&
+        board[combination[2]] === 2
+      ) {
+        window.alert("BOT WINS!");
+        return;
+      }
     }
-  }
+  };
 
-  const WIN_COMBINATIONS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
-    [0, 4, 8], [2, 4, 6], //diagonals
-  ];
+  const changeTurn = () =>
+    (currentTurn = currentTurn === "player-turn" ? "bot-turn" : "player-turn");
 
   return {
     generateBoard: function () {
@@ -50,20 +70,30 @@ function Gameboard() {
         gameboardCell.classList.add("cell");
         gameboardDiv.appendChild(gameboardCell);
 
-        //creating the onclick event for each
-        gameboardCell.addEventListener('click', () => {
-          //if the value in board array of the clicked cell is not 0. It means that its taken.
-          if (board[index] === 0){
+        //creating the onclick event for each cell element.
+        gameboardCell.addEventListener("click", () => {
+          // If the value in board array of the clicked cell is not 0. It means that its taken.
+          if (board[index] === 0) {
+            if (currentTurn === "player-turn") {
+              gameboardCell.classList.add("player-cell");
+            } else {
+              gameboardCell.classList.add("bot-cell");
+            }
             setBoardValue(index);
-          }
-          else {
+          } else {
             console.log("Can't change cell value!");
           }
-        })
+        });
       });
     },
   };
 }
 Gameboard().generateBoard();
-
-//each cell could be an object containing information like its index, cell value (empty, player or bot).
+/*
+  TODO:
+  1 - [DONE] Implement winning condition. (check the test-project, use the winning combinations).
+  2 - Create logic to control the style of each cell depending on who owns it.
+  3 - Style each cell onclick.
+  4 - Find a way to display a "O" or an "X".
+  5 - Implement bot logic.
+*/
